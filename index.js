33940d244d9ec885e43c4cccb95aae86f52113c4
ghemsley <ghemsley@protonmail.ch>
2021-05-18 15:33:30 -0500

    commit tree 56aad8c501e35891f30985964b5669939a7d6dbb

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+function saturdayFun(activity = 'roller-skate') {
+  return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function (activity = 'go to the office') {
+  return `This Monday, I will ${activity}.`
+}
+
+const wrapAdjective = (flair = '*') => {
+  return (string = 'special') => {
+    return `You are ${flair}${string}${flair}!`
+  }
+}
+
+const Calculator = {
+  add: () => 1 + 3,
+  subtract: () => 1 - 3,
+  multiply: () => 1 * 3,
+  divide: () => 10 / 5
+}
+
+const actionApplyer = (start, array) => {
+  if (array.length < 1) {
+    return start
+  } else {
+    let firstRun = false
+    let result
+    array.forEach((element) => {
+      if (firstRun === false) {
+        result = element(start)
+        firstRun = true
+      } else {
+        result = element(result)
+      }
+    })
+    return result
+  }
+}
